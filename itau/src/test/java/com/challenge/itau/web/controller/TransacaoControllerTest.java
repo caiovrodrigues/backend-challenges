e7c70f01a06c9ffb66964e9e10ddfdd432a79b47
Caio Rodrigues <caiobiscas@gmail.com>
2025-04-17 14:12:05 -0300

    Itaú - Add unit test for TransacaoController create method

--- itau/src/test/java/com/challenge/itau/web/controller/TransacaoControllerTest.java
+++ itau/src/test/java/com/challenge/itau/web/controller/TransacaoControllerTest.java
@@ -1,6 +1,8 @@
 package com.challenge.itau.web.controller;
 
 import com.challenge.itau.service.TransacaoService;
+import com.challenge.itau.web.dto.TransacaoInput;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -8,7 +10,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 
 @ExtendWith(MockitoExtension.class)
 class TransacaoControllerTest {
@@ -18,6 +25,25 @@ class TransacaoControllerTest {
 
     @InjectMocks
     TransacaoController transacaoController;
+
+    @Nested
+    class create{
+
+        @Test
+        @DisplayName("Should delegate create transaction responsibility to TransacaoService")
+        void shouldDelegateCreateResponsibilityToTransacaoService(){
+            //Arrange
+            var input = new TransacaoInput(BigDecimal.valueOf(1_000), OffsetDateTime.now());
+
+            //Act
+            var response = transacaoController.create(input);
+
+            //Assert
+            Assertions.assertThat(response.getStatusCode().value()).isEqualTo(HttpStatus.CREATED.value());
+            verify(transacaoService, times(1)).create(input);
+            verifyNoMoreInteractions(transacaoService);
+        }
+    }
 
     @Nested
     class delete{
